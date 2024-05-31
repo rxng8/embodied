4e2bedbc5073495b8d6cd04d9142309853dbc28c
Viet Dung Nguyen <alexvn.edu@gmail.com>
2024-05-31 12:03:28 -0400

    initial push

---
+++ embodied/__init__.py
@@ -0,0 +1,20 @@
+__version__ = '0.0.1'
+
+from .core import *
+
+from . import distr
+from . import envs
+from . import replay
+from . import run
+
+try:
+  from rich import traceback
+  import numpy as np
+  import jax
+
+  traceback.install(
+      # show_locals=True,
+      suppress=[np, jax])
+
+except ImportError:
+  pass
