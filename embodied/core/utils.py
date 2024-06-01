05014361c02af41010230409d4e30b3dc8162d0c
Viet Dung Nguyen <alexvn.edu@gmail.com>
2024-06-01 14:21:32 -0400

    check vscode interactive

--- embodied/core/utils.py
+++ embodied/core/utils.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-
+import sys
 
 def timestamp(now=None, millis=False):
   now = datetime.now() if now is None else now
@@ -7,3 +7,20 @@ def timestamp(now=None, millis=False):
   if millis:
     string += f'F{now.microsecond:06d}'
   return string
+
+
+def check_vscode_interactive() -> bool:
+  if hasattr(sys, 'ps1'):
+    return True # ipython on Windows or WSL
+  else: # check on linux: https://stackoverflow.com/a/39662359
+    try:
+      shell = get_ipython().__class__.__name__
+      if shell == 'ZMQInteractiveShell':
+        return True   # Jupyter notebook or qtconsole
+      elif shell == 'TerminalInteractiveShell':
+        return False  # Terminal running IPython
+      else:
+        return False  # Other type (?)
+    except NameError:
+      return False      # Probably standard Python interpreter
+
