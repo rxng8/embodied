6b17fa37f9602b87e6dc77a60d894cafac0b7171
Viet Nguyen <alexvn.edu@gmail.com>
2024-06-21 05:30:30 -0400

    update lib

--- embodied/nn/__init__.py
+++ embodied/nn/__init__.py
@@ -2,5 +2,7 @@ from .base import *
 from .distributions import *
 from .functional import *
 from .jaxutils import *
+from .ninjax import (pure, init, seed, grad, cond, scan, Module,
+  Variable, FromFlax, FromHaiku, FromOptax)
 from . import ninjax
 from .optimizers import *
